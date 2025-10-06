3ef5bd9b132728a1e54ffea03765c4a95d54c2ae
ben_1 <ben@streamlinedanalytics.co.uk>
2025-10-06 14:29:50 +0100

    first awesome commit

---
+++ opensearch_filtering/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from opensearch_filtering.users.models import User
+from opensearch_filtering.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
